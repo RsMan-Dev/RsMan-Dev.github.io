47f0ce58e43549f37e3261902fd176c7cf19f9f6
RsMan-Dev <contact@rsman.fr>
2025-05-01 22:34:49 +0200

    rebuild

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -4,8 +4,11 @@ import "./Counter.css";
 export default function Counter() {
   const [count, setCount] = createSignal(0);
   return (
-    <button class="increment" onClick={() => setCount(count() + 1)} type="button">
-      Clicks: {count()}
-    </button>
+    <div>
+      <h1>Counter</h1>
+      <button class="increment" onClick={() => setCount(count() + 1)} type="button">
+        Clicks: {count()}
+      </button>
+    </div>
   );
 }
