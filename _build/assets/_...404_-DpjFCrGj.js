f337c2249471e1addcffc5ec82930f8597cdbe15
RsMan-Dev <58441276+RsMan-Dev@users.noreply.github.com>
2025-05-01 20:16:51 +0000

    Deploying to gh-pages from @ RsMan-Dev/RsMan-Dev.github.io@d111663a6882ee3fe6c373c296de2111ca682e95 🚀

--- _build/assets/_...404_-DpjFCrGj.js
+++
@@ -1,3 +0,0 @@
-import{M as r}from"./index-BmnT0ak3.js";import{j as n,y as s}from"./web-D0I6bkB-.js";import{H as a}from"./HttpStatusCode-DjTx85av.js";function o(t){const e=Object.assign({h1:"h1",p:"p",a:"a"},r(),t.components);return[n(a,{code:404}),`
-`,n(e.h1,{children:"Page Not Found"}),`
-`,n(e.p,{get children(){return["Visit ",n(e.a,{href:"https://solidjs.com",children:"https://solidjs.com"})," to learn how to build Solid apps."]}})]}function m(t={}){const{wrapper:e}=Object.assign({},r(),t.components);return e?n(e,s(t,{get children(){return n(o,t)}})):o(t)}export{m as default};
